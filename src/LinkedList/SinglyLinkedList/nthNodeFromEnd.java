054536a77311b4f7eac8cfbbb19e2dc40f5a7e3f
dr-momin <mohammadmomin00@gmail.com>
2023-04-18 18:34:18 -0400

    LC Q1 started..

--- src/LinkedList/SinglyLinkedList/nthNodeFromEnd.java
+++ src/LinkedList/SinglyLinkedList/nthNodeFromEnd.java
@@ -1,7 +1,24 @@
 package LinkedList.SinglyLinkedList;
 
 public class nthNodeFromEnd {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+
     public static void main(String[] args) {
+
+        // Question from LeetCode
+        // Solution
+
+
+
 
     }
 }
