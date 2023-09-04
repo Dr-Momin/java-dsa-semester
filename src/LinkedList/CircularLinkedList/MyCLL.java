e121c30266882b59216756b9193a7f1da08693ac
Dr-Momin <mohammadmomin00@gmail.com>
2023-09-04 17:53:10 +0500

    ADD: Circular Linked List

--- src/LinkedList/CircularLinkedList/MyCLL.java
+++ src/LinkedList/CircularLinkedList/MyCLL.java
@@ -16,6 +16,45 @@ public class MyCLL {
 
     public static class CLL{
 
+        Node head, tail;
+
+
+        public void insertAtEnd(int data){
+            Node newNode = new Node(data);
+
+            if(head == null){
+                head = tail = newNode;
+                return;
+            }
+
+
+            tail.next = newNode;
+            tail = newNode;
+            newNode.next = head;
+
+
+        }
+
+        public void insertAtHead(int data){
+
+        }
+
+
+        public void deleteHeadWithoutTail(){
+            Node currNode = head;
+
+
+        }
+
+        public void display(){
+            Node currNode = head;
+            do{
+                System.out.print(currNode.data + " ");
+                currNode = currNode.next;
+
+            }while (currNode != head);
+
+        }
 
     }
 
@@ -23,7 +62,14 @@ public class MyCLL {
     public static void main(String[] args) {
 
 
+        CLL list = new CLL();
 
+        list.insertAtEnd(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtEnd(40);
+
+        list.display();
 
 
     }
