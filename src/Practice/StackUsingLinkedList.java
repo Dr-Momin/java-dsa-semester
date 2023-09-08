31d08c5b35c32811efa892d6d5905aa8f0081e76
Dr-Momin <mohammadmomin00@gmail.com>
2023-09-08 21:56:40 +0500

    Stack using LinkedList

--- src/Practice/StackUsingLinkedList.java
+++ src/Practice/StackUsingLinkedList.java
@@ -1,2 +1,27 @@
-package Practice;public class StackUsingLinkedList {
+package Practice;
+
+public class StackUsingLinkedList {
+
+    public static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static class MyStack{
+        Node head;
+
+        public void push(){
+            
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
