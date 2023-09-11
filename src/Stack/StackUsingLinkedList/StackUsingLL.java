cb1f1cafb1f4bee5dfcd914bb09b0dde5b87a859
Dr-Momin <mohammadmomin00@gmail.com>
2023-09-12 01:08:01 +0500

    minor refactoring

--- src/Stack/StackUsingLinkedList/StackUsingLL.java
+++ src/Stack/StackUsingLinkedList/StackUsingLL.java
@@ -4,16 +4,10 @@ public class StackUsingLL {
 
 
     public static class StackLL{
-
         Node head;
         int size;
 
-        StackLL(){
-            head = null;
-            size = 0;
-        }
-
-        void push(int data){
+        public void push(int data){
             Node newNode = new Node(data);
 
             if(head == null){
@@ -26,7 +20,8 @@ public class StackUsingLL {
 
         }
 
-        int pop(){
+
+        public int pop(){
             if (head == null){
                 System.out.println("List is Empty");
                 return -1;
@@ -39,52 +34,24 @@ public class StackUsingLL {
         }
 
 
-//
-//        StackLL(){
-//            head = null;
-//            size = 0;
-//        }
-//
-//        void push(int data){
-//            Node temp = new Node(data);
-//            temp.next = head;
-//            head = temp;
-//
-//            size++;
-//        }
-//
-//
-//        int peek(){
-//            if(head == null){
-//                System.out.println("Stack is Empty!!");
-//                return -1;
-//            }
-//
-//            return head.data;
-//
-//        }
-//
-//        int pop(){
-//            if(head == null){
-//                System.out.println("Stack is Empty!!");
-//                return -1;
-//            }
-//
-//            int result = head.data;
-//            head = head.next;
-//            size--;
-//
-//            return result;
-//        }
-//
-//
-//        boolean isEmpty(){
-//            return head == null;
-//        }
-//
-//        int size(){
-//            return size;
-//        }
+        public int peek(){
+            if(head == null){
+                System.out.println("Stack is Empty!!");
+                return -1;
+            }
+
+            return head.data;
+
+        }
+
+
+        public boolean isEmpty(){
+            return head == null;
+        }
+
+        public int size(){
+            return size;
+        }
 
     }
 
