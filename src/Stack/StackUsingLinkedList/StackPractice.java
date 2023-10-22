a58c6bf941bee4512a056c658ccfabe0b11fb709
Dr-Momin <mohammadmomin00@gmail.com>
2023-10-22 09:34:03 +0500

    stack practice

--- src/Stack/StackUsingLinkedList/StackPractice.java
+++ src/Stack/StackUsingLinkedList/StackPractice.java
@@ -1,5 +1,7 @@
 package Stack.StackUsingLinkedList;
 
+import java.util.Stack;
+
 public class StackPractice {
 
 
@@ -62,20 +64,113 @@ public class StackPractice {
 
 
     // TODO: Advance Functions
+    static void copyStackIntoAnotherStackInSameOrder(StackPractice s1, StackPractice s2){
 
+        StackPractice s3 = new StackPractice();
+
+        while (s1.length() > 0){
+            s3.push(s1.pop());
+        }
+
+        while (s3.length() > 0){
+            s2.push(s3.pop());
+        }
+
+    }
+
+    void pushAtBottom(int data){
+        Node newNode = new Node(data);
+        StackPractice st = new StackPractice();
+
+        // Pop until the last element
+        while (length() > 0){
+            st.push(pop());
+        }
+
+        // pushing at Bottom
+        push(data);
+
+        // copying back the stack
+        while (st.length() > 0){
+            push(st.pop());
+        }
+    }
+
+
+    void popFromBottom(){
+        StackPractice helper = new StackPractice();
+
+        // pop until last element
+        while (length() > 1){
+            helper.push(pop());
+        }
+
+        // deleting the last element
+        pop();
+
+        // copying back the stack
+        while (helper.length() > 0){
+            push(helper.pop());
+        }
+
+    }
+
+
+    static void balancedBrackets(String word){
+        char c; Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < word.length(); i++) {
+            c = word.charAt(i);
+            if (c == '(' || c == '['){
+                st.push(c);
+            }
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            c = word.charAt(i);
+
+            if ((c == ')' && st.peek() == '(') || (c == ']' && st.peek() == '[') ){
+                st.pop();
+            }
+        }
+
+        if (st.isEmpty()){
+            System.out.println("Balanced....");
+        }else {
+            System.out.println("Not Balanced....");
+        }
+    }
 
 
     public static void main(String[] args) {
 
         StackPractice stack01 = new StackPractice();
+        StackPractice stack02 = new StackPractice();
+
         stack01.push(10);
         stack01.push(20);
         stack01.push(30);
+        stack01.push(40);
+//        stack01.display();
 
-        stack01.display();
 
-        System.out.println(stack01.length());
+//        copyStackIntoAnotherStackInSameOrder(stack01, stack02);
+//        stack02.display();
 
+//        stack01.pushAtBottom(90);
+//        stack01.display();
+//
+//        stack01.popFromBottom();
+//        stack01.display();
+
+
+        String str = "(3+4)";
+        String str2 = "[2+(3+4)+(2+1)]";
+        String str3 = "[2+(3+4)+(2+1)]";
+        String str4 = "[()]";
+        String str5 = "([)]";
+
+        balancedBrackets(str);
 
 
     }
